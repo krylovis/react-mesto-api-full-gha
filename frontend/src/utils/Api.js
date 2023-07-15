15c31b563007480724ff99be9f2ac6bb0e8bf78a
Ivan Krylov <kis.226@yandex.ru>
2023-07-15 16:54:49 +0300

    delete

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -80,13 +80,9 @@ class Api {
   }
 };
 
-const cohort = 'cohort-63';
-const token = '14893dca-a279-433c-a27c-967896487d71';
-// const baseUrl = `https://mesto.nomoreparties.co/v1/${cohort}`;
 const baseUrl = 'https://api.krylovis.students.nomoredomains.xyz';
 
 const headers = {
-  // authorization: token,
   'Content-Type': 'application/json',
 };
 
