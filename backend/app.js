14a1357b5ef9b00df5ba70853f9fb7d6f8e7ac40
Ivan Krylov <kis.226@yandex.ru>
2023-07-15 15:46:49 +0300

    add dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
-const { PORT = 3000 } = process.env;
+const {
+  PORT = 3000,
+  MONGO_URL = 'mongodb://localhost:27017',
+} = process.env;
 const routes = require('./routes');
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect(`${MONGO_URL}/mestodb`);
 
 app.use(express.json());
 app.use(routes);
