6ff2d44060e0effb68729fb5464abf1ecf2a4387
Ivan Krylov <kis.226@yandex.ru>
2023-07-16 15:16:04 +0300

    1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const { requestLogger, errorLogger } = require('../middlewares/logger');
 // const { corsSettings } = require('../middlewares/corsSettings');
+
 const NotFoundError = require('../custom-errors/NotFoundError');
 
 const { PAGE_NOT_FOUND } = require('../utils/constants');
