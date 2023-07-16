d48cb166a045751ac23339b0ca419e2278f1b705
Ivan Krylov <kis.226@yandex.ru>
2023-07-16 12:26:34 +0300

    generateToken

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,4 +1,8 @@
 const jwt = require('jsonwebtoken');
 
-const JWT_SECRET = 'super-strong-secret';
-module.exports.generateToken = (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: '7d' });
+const { NODE_ENV, JWT_SECRET } = process.env;
+module.exports.generateToken = (id) => jwt.sign(
+  { id },
+  NODE_ENV === 'production' ? JWT_SECRET : 'super-strong-secret',
+  { expiresIn: '7d' },
+);
