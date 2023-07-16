49f8aef3ae76fc9a92968f36d0956f70e9854512
Ivan Krylov <kis.226@yandex.ru>
2023-07-16 15:09:16 +0300

    cors test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const { requestLogger, errorLogger } = require('../middlewares/logger');
-const { corsSettings } = require('../middlewares/corsSettings');
+// const { corsSettings } = require('../middlewares/corsSettings');
 const NotFoundError = require('../custom-errors/NotFoundError');
 
 const { PAGE_NOT_FOUND } = require('../utils/constants');
@@ -10,10 +11,26 @@ const usersRoutes = require('./usersRoutes');
 const cardsRoutes = require('./cardsRoutes');
 const authRoutes = require('./authRoutes');
 
+const allowedCors = [
+  'http://mesto.krylovis.students.nomoredomains.xyz',
+  'https://mesto.krylovis.students.nomoredomains.xyz',
+  'http://api.krylovis.students.nomoredomains.xyz',
+  'https://api.krylovis.students.nomoredomains.xyz',
+  'localhost:3000',
+];
+
 router.use(requestLogger);
 
-router.use(corsSettings);
 router.use(cookieParser());
+router.options('*', cors({
+  origin: allowedCors,
+  credentials: true,
+}));
+router.use(cors({
+  origin: allowedCors,
+  credentials: true,
+}));
+// router.use(corsSettings);
 router.use('/', authRoutes);
 router.use('/users', usersRoutes);
 router.use('/cards', cardsRoutes);
