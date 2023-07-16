09a3b547caaed3ef5c38e2f19bc5f0c44894d2c1
Ivan Krylov <kis.226@yandex.ru>
2023-07-16 14:23:15 +0300

    corsSettings

--- backend/middlewares/corsSettings.js
+++ backend/middlewares/corsSettings.js
@@ -10,14 +10,13 @@ const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports.corsSettings = (req, res, next) => {
   const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
-
-  const { method } = req;
-  const requestHeaders = req.headers['access-control-request-headers'];
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
