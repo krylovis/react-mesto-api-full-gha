e535de0a0000bf98182ae2a3a249458f62dd5371
Ivan Krylov <kis.226@yandex.ru>
2023-07-16 12:16:42 +0300

    getUser

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -7,7 +7,8 @@ const {
 } = require('../controllers/usersController');
 
 router.get('/', auth, getUsers);
-router.get('/me', auth, getUser);
+// router.get('/me', auth, getUser);
+router.get('/me', getUser);
 router.get('/:id', auth, getUserSchema, getUserById);
 
 router.patch('/me', auth, updateUserSchema, updateUser);
