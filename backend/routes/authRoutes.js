ffb9bdaebe3ed9945a0d9a87cc7c8b782b29c32a
Krylov Ivan <krylovis@mchs.ru>
2023-07-18 12:54:47 +0300

    add crash-test

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 const { createUser, login, logout } = require('../controllers/usersController');
 const { createUserSchema, loginSchema } = require('../middlewares/joiSchemas');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 router.post('/signup', createUserSchema, createUser);
 router.post('/signin', loginSchema, login);
 router.post('/logout', logout);
